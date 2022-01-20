2bd058e2609f6b6181a57e46d01f4a0cbae38780
Massimo Tagliavini <massimo.tagliavini@gmail.com>
2022-01-20 21:08:01 +0100

    little refinement

--- assets/js/sortable-grid-view.js
+++ assets/js/sortable-grid-view.js
@@ -1,82 +1,83 @@
 (function ($) {
-    $.SortableGridView = function (options) {
-	var defaultOptions = {
-	    id: 'sortable-grid-view',
-	    action: 'sortItem',
-	    sortingPromptText: 'Loading...',
-	    sortingFailText: 'Fail to sort',
-	    moveItem: '',
-	    csrfTokenName: '',
-	    csrfToken: '',
-	};
+	$.SortableGridView = function (options) {
+		var defaultOptions = {
+			id: 'sortable-grid-view',
+			action: 'sortItem',
+			sortingPromptText: 'Loading...',
+			sortingFailText: 'Fail to sort',
+			moveItem: '',
+			csrfTokenName: '',
+			csrfToken: '',
+		};
 
-	$.extend({}, defaultOptions, options);
+		$.extend({}, defaultOptions, options);
 
-	$('body').append('<div class="modal fade" id="' + options.id + '-sorting-modal" tabindex="-1" role="dialog"><div class="modal-dialog"><div class="modal-content"><div class="modal-body">' + options.sortingPromptText + '</div></div></div></div>');
+		$('body').append('<div class="modal fade" id="' + options.id + '-sorting-modal" tabindex="-1"><div class="modal-dialog"><div class="modal-content"><div class="modal-body">' + options.sortingPromptText + '</div></div></div></div>');
 
-	var regex = /items\[\]\_(\d+)/;
+		// var regex = /items\[\]\_(\d+)/;
+		var loadingModal = $('#' + options.id + '-sorting-modal');
 
-	$('#' + options.id + ' .sortable-grid-view tbody').sortable({
-		handle: options.moveItem,
-	    update : function () {
-		$('#' + options.id + '-sorting-modal').modal('show');
-		//serial = $('#' + options.id + ' .sortable-grid-view tbody').sortable('toArray', {attribute:'id'});
-        serial = [];
+		$('#' + options.id + ' .sortable-grid-view tbody').sortable({
+			handle: options.moveItem,
+			update: function () {
+				loadingModal.modal('show');
+				//serial = $('#' + options.id + ' .sortable-grid-view tbody').sortable('toArray', {attribute:'id'});
+				serial = [];
 
-
-        $('#' + options.id + ' .sortable-grid-view tbody.ui-sortable tr').each( function() {
-            //console.log($(this).data('key'));
-            serial.push($(this).data('key'));
-        });
-
-        //console.log(serial);
-
-
-		var length = serial.length;
-		var currentRecordNo = 0;
-		var successRecordNo = 0;
-		var data = [];
-
-		if(length > 0){
-		    for(var i=0; i<length; i++){
-			//var itemID = regex.exec(serial[i]);
-            var itemID = serial[i];
-			//data = data + 'items[' + i + ']=' + itemID[1] + '&';
-            data.push(itemID)
-			currentRecordNo++;
-
-			if(currentRecordNo == 500 || i == (length-1)){
-			    //data =  data + options.csrfTokenName + '=' + options.csrfToken;
-
-			    (function(currentRecordNo){
-				$.ajax({
-				    'url': options.action,
-				    'type': 'post',
-				    'data': { 'items': data, [options.csrfTokenName] : options.csrfToken },
-				    success: function(data){
-					                   checkSuccess(currentRecordNo);
-				    },
-				    error: function(data){
-					$('#' + options.id + '-sorting-modal').modal('hide');
-					alert(options.sortingFailText);
-				    }
+				$('#' + options.id + ' .sortable-grid-view tbody.ui-sortable tr').each(function () {
+					//console.log($(this).data('key'));
+					serial.push($(this).data('key'));
 				});
-			    })(currentRecordNo);
 
-			    currentRecordNo = 0;
-			    data = [];
-			}
-		    }
-		}
+				//console.log(serial);
 
-		function checkSuccess(count){
-		    successRecordNo += count;
+				var length = serial.length;
+				var currentRecordNo = 0;
+				var successRecordNo = 0;
+				var data = [];
 
-		    if(successRecordNo >= length){
-			$('#' + options.id + '-sorting-modal').modal('hide');
-		    }
-		}
-	    },
-	});
-    };
+				if (length > 0) {
+					for (var i = 0; i < length; i++) {
+						//var itemID = regex.exec(serial[i]);
+						var itemID = serial[i];
+						//data = data + 'items[' + i + ']=' + itemID[1] + '&';
+						data.push(itemID)
+						currentRecordNo++;
+
+						if (currentRecordNo == 500 || i == (length - 1)) {
+							//data =  data + options.csrfTokenName + '=' + options.csrfToken;
+
+							(function (currentRecordNo) {
+								$.ajax({
+									'url': options.action,
+									'type': 'post',
+									'data': { 'items': data, [options.csrfTokenName]: options.csrfToken },
+									success: function (data) {
+										checkSuccess(currentRecordNo);
+									},
+									error: function (data) {
+										$('#' + options.id + '-sorting-modal').modal('hide');
+										alert(options.sortingFailText);
+									}
+								});
+							})(currentRecordNo);
+
+							currentRecordNo = 0;
+							data = [];
+						}
+					}
+				}
+
+				function checkSuccess(count) {
+					successRecordNo += count;
+					if (successRecordNo >= length) {
+
+						loadingModal.on('shown.bs.modal', function () {
+							loadingModal.modal('hide');
+						});
+					}
+				}
+			},
+		});
+	};
 })(jQuery);
